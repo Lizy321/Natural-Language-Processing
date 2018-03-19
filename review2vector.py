3de28f1da541f019812d87773950e3f9c0784383
Lizy321 <32933043+Lizy321@users.noreply.github.com>
2018-03-19 13:01:00 +0100

    Add files via upload

--- review2vector.py
+++ review2vector.py
@@ -4,8 +4,12 @@ import numpy as np
 from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from nltk import word_tokenize, pos_tag
 from pandas import DataFrame
 import re
+import nltk
+#nltk.download()
 
 import csv
 from textblob.classifiers import NaiveBayesClassifier
@@ -33,23 +37,64 @@ doc_terms_train, doc_terms_test, y_train, y_test \
     = train_test_split(sentences, movie_reviews.Sentiment, test_size=0.3)
 
 '''''BOOL型特征下的向量空间模型，注意，测试样本调用的是transform接口'''
+# unigram
 count_vec = TfidfVectorizer(binary=False, decode_error='ignore', \
                             stop_words='english')
 
-# training set
-x_train = count_vec.fit_transform(doc_terms_train)
-# LABEL --> y_train
+#bigram
 
-#testing set
-x_test = count_vec.transform(doc_terms_test)
-#Label --> y_test
+#print(vectorizer.get_feature_names())
+
+# review2vector -- transforming review to vector where num as the length of each tokens
+def review2vector_tfidf(range):
+    vectorizer = TfidfVectorizer(max_features=40000, ngram_range=range , sublinear_tf=True, binary=False, decode_error='ignore', \
+                            stop_words='english')#ngram_range=(1, 3)
+
+    x_train = vectorizer.fit_transform(doc_terms_train)
+    x_test = vectorizer.fit_transform(doc_terms_test)
+
+    print("feature names:")
+    print(vectorizer.get_feature_names())
+
+    return x_train,x_test
+
+x_train_uni, x_test_uni = review2vector_tfidf((1,1))
+x_train_bi, x_train_bi = review2vector_tfidf((2,2))
+
+x_train_mix, x_train_mix = review2vector_tfidf((1,2))
 
 
-x = count_vec.transform(sentences)
-y = movie_reviews.Sentiment
-print(doc_terms_train)
-print(count_vec.get_feature_names())
-print(x_train.toarray())
-print(movie_reviews.target)
 
-#text_clf = x_train.fit(OneVsOneClassifier(LinearSVC()),np.asarray())
+
+# POS feature
+
+
+def pos_feature(new_terms_train):
+    pos_review = []
+    for review in new_terms_train:
+        tokens = word_tokenize(review)
+        tags = dict(pos_tag(tokens)).values()
+        pos = ' '.join(list(tags))
+        pos_review.append(review + ' ' + pos)
+    return pos_review
+
+pos_train = pos_feature(doc_terms_train)
+pos_test = pos_feature(doc_terms_test)
+
+
+
+
+def POSreview2vector_tfidf(range):
+    vectorizer = TfidfVectorizer(max_features=40000, ngram_range=range , sublinear_tf=True, binary=False, decode_error='ignore', \
+                            stop_words='english')#ngram_range=(1, 3)
+
+    x_train = vectorizer.fit_transform(pos_train)
+    x_test = vectorizer.fit_transform(pos_test)
+
+    print("feature names:")
+    print(vectorizer.get_feature_names())
+
+    return x_train,x_test
+
+
+pos_x_train, pos_x_test= POSreview2vector_tfidf((1,1))
